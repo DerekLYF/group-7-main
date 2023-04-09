f978bd6ea18445c0acb86333af709cfe364c1da5
DerekLYF <97366511+DerekLYF@users.noreply.github.com>
2023-04-10 05:46:15 +0800

    trying to add animation

---
+++ Assets/mother/FemaleCharacterController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class FemaleCharacterController : MonoBehaviour
+{
+    public GameObject destinationPoint;
+    public float walkSpeed = 1.0f;
+
+    private Animator animator;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        navMeshAgent.speed = walkSpeed;
+        navMeshAgent.SetDestination(destinationPoint.transform.position);
+        animator.SetFloat("Speed", walkSpeed);
+    }
+
+    void Update()
+    {
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            animator.SetFloat("Speed", 0);
+        }
+    }
+}
